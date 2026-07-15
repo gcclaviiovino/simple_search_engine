5f58c1ce889f6ec4fa217a4c1208425e226f2352
Lavi Iovino <lalliovino@gmail.com>
2026-07-15 11:08:58 +0200

    add: file retrieval logic in main, runEngine compiling rule in build file; task: stage 5 complete

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,3 +20,17 @@ kotlin {
 tasks.test {
     useJUnitPlatform()
 }
+
+tasks.register<JavaExec>("runEngine") {
+    group = "application"
+    description = "Runs the search engine with arguments"
+
+    dependsOn("classes")
+    mainClass.set("MainKt")
+
+    // Use project.the to safely extract the runtime classpath
+    val sourceSets = project.the<SourceSetContainer>()
+    classpath = sourceSets["main"].runtimeClasspath
+
+    standardInput = System.`in`
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,17 @@
-fun main() {
-    println("Welcome, please load your data before you start.")
-    val dataset = getData()
+import java.io.File
+
+fun main(args: Array<String>) {
+    if (!(args.any { word -> word == "--data" })) {
+        println("To load data use flag --data before file. Please try again.")
+        return
+    }
+    val fileName = args.lastOrNull()
+    if (fileName == null) {
+        println("Data file needed to start. Please try again.")
+        return
+    }
+
+    val dataset = getVals(File(fileName).readLines())
     if (dataset.isEmpty() || dataset.rowsEmpty()) {
         println("Dataset can't be empty or incomplete. Please restart.")
         return
